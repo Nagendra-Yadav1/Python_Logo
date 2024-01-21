6c73eb67dba439932f6bc812d57de3dec8d9a346
Nagendra-Yadav1 <nagendrayadav13938@gmail.com>
2024-01-21 12:18:21 +0530

    Pthon-Logo

---
+++ logo.py
@@ -0,0 +1,16 @@
+# python logo
+# for x in range(21):
+#     for y in range(21):
+#         if ((y==6) and (x>0 and x<5)) or ((x==0) and (y>6 and y<14)) or ((y==14) and (x>0 and x<10)) or ((x==4) and (y>6 and y<10)) or ((y==9) and ((x==5) or (x==6))):
+#             print("* ",end="")
+#         elif ((x==6) and (y>0 and y<10)) or ((y==0)  and (x>6 and x<14)) or ((x==10 ) and (y>5 and y<14)) or ((y==5) and (x>10 and x<14)) or ((x==14) and (y>0 and y<5)):
+#             print("* ",end="")
+#         elif ((x==2) and ((y==8))) or ((x==18) and y==14):
+#             print("* ",end="")
+#         elif ((x==6) and (y>15 and y<20)) or ((y==15) and (x>6 and x<11)) or ((x==11) and (y>6 and y<15)) or ((y==20) and (x>6 and x<14)) or ((y==6) and x>11 and x<20) or ((x==14) and (y>10 and y<20)) or ((x==20) and (y>6 and y<16)) :
+#             print("* ",end="")
+#         elif ((y==11) and x==15) or ((x==16) and (y>10 and y<16)) or ((y==16) and (x>15 and x<20)):
+#             print("* ",end="")
+#         else:
+#             print("  ",end="")
+#     print("")
